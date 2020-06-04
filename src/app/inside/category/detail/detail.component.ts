c5699cdbab8b9686c832798ea9b39de16ad3d832
maintth1807076 <maintth1807076@fpt.edu.vn>
2020-06-04 15:35:27 +0700

    crud category

--- src/app/inside/category/detail/detail.component.ts
+++ src/app/inside/category/detail/detail.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {AngularFirestore} from '@angular/fire/firestore';
+import {ActivatedRoute} from '@angular/router';
+import {takeWhile} from 'rxjs/operators';
 
 @Component({
   selector: 'app-detail',
@@ -7,9 +10,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DetailCategoryComponent implements OnInit {
 
-  constructor() { }
+  idCategory: string;
+  id: any;
+  p: any;
+  category: any = {};
+  courses: any = [];
+  alive: boolean = true;
+  constructor(public afs: AngularFirestore, private route: ActivatedRoute) {
+    this.idCategory = this.route.snapshot.paramMap.get('id');
+    if(this.idCategory != null && this.idCategory.length > 0){
+      this.getDetail(this.idCategory);
+      this.getCourses(this.idCategory)
+    }
+  }
 
   ngOnInit(): void {
   }
 
+  getDetail(id){
+    this.afs.collection('categories').doc(id).get().pipe(takeWhile(() => this.alive))
+      .subscribe(doc => {
+        this.category = doc.data()
+      })
+  }
+
+  getCourses(id){
+    this.afs.collection('courses', ref => ref.where('categoryId', '==', id)).valueChanges()
+      .pipe(takeWhile(() => this.alive))
+      .subscribe(data => {
+        this.courses = data;
+      })
+  }
 }
