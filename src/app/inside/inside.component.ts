a1700feec2776c634d7b191edd0b23e55811832c
thi <thittth1807067@fpt.edu.vn>
2020-06-08 15:14:50 +0700

    giao dien inside

--- src/app/inside/inside.component.ts
+++ src/app/inside/inside.component.ts
@@ -10,5 +10,20 @@ export class InsideComponent implements OnInit {
 
   ngOnInit(): void {
   }
-
+  async ngAfterViewInit() {
+    // <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js" type="text/javascript"></script>
+    await this.loadScript('https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js');
+    await this.loadScript('/assets/js/jquery.min.js');
+    await this.loadScript('/assets/js/popper.js');
+    await this.loadScript('/assets/js/bootstrap.min.js');
+    await this.loadScript('/assets/js/main.js');
+  }
+  loadScript(scriptUrl: string) {
+    return new Promise((resolve, reject) => {
+      const scriptElement = document.createElement('script');
+      scriptElement.src = scriptUrl;
+      scriptElement.onload = resolve;
+      document.body.appendChild(scriptElement);
+    })
+  }
 }
