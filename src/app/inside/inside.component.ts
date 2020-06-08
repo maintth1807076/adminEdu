284cd3810d6bd511cbdbd9c8a597103f3df1b899
thi <thittth1807067@fpt.edu.vn>
2020-06-08 15:43:36 +0700

    giao dien inside

--- src/app/inside/inside.component.ts
+++ src/app/inside/inside.component.ts
@@ -4,6 +4,7 @@ import { Component, OnInit } from '@angular/core';
   selector: 'app-inside',
   templateUrl: './inside.component.html',
   styleUrls: ['./inside.component.css']
+  
 })
 export class InsideComponent implements OnInit {
   constructor() { }
